05c4521d0ebbd965d0d2cf254a0b8848b03fc15a
Vlad Pronsky <v.pronsky@gmail.com>
2024-02-03 08:10:57 +0200

    fix share button

--- src/components/ChangeBoardDate.tsx
+++ src/components/ChangeBoardDate.tsx
@@ -1,12 +1,14 @@
 import { useAtomValue, useSetAtom } from "jotai"
 import { FC } from "react"
-import { ComputedDays, SelectedDate } from "../store"
+import { ActiveTab, ComputedDays, SelectedDate } from "../store"
+import { encodeShareUrl } from "../utils/share"
 import { Button } from "./ui/Button"
 import { ButtonCopy } from "./ui/ButtonCopy"
 
 export const ChangeBoardDate: FC = () => {
   const setDate = useSetAtom(SelectedDate)
   const dates = useAtomValue(ComputedDays)
+  const activeTab = useAtomValue(ActiveTab)
 
   return (
     <div className="flex grow justify-between gap-1">
@@ -22,7 +24,7 @@ export const ChangeBoardDate: FC = () => {
       </div>
 
       <div className="flex items-center gap-2.5">
-        <ButtonCopy value={() => dates.map((x) => x.date).join(" ")} size="sm">
+        <ButtonCopy value={() => encodeShareUrl(activeTab)} size="sm">
           Share
         </ButtonCopy>
       </div>
